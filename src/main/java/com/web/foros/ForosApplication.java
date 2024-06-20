381be2739646412e79dd68a404ae6b703132cd10
inaki Bengeochea <bengoinaki@gmail.com>
2024-06-20 16:23:34 -0300

    commit init

---
+++ src/main/java/com/web/foros/ForosApplication.java
@@ -0,0 +1,13 @@
+package com.web.foros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/foros/ForosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.foros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
